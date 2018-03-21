a237f92819945f7c4c02580b5670fa42c7a54b30
Mihaela <mihaelaagrigore@gmail.com>
2018-03-21 19:57:50 +0200

    first commit

--- index.js
+++ index.js
@@ -3,21 +3,9 @@ var app = express();
 var path = require('path');
 var gpio = require('rpi-gpio');
 
-//gpio.setup(7, gpio.DIR_OUT);
-gpio.setup(7, gpio.DIR_IN, readInput);
+gpio.setup(7, gpio.DIR_OUT);
 
-function readInput(err) {
-    if (err) throw err;
-    gpio.read(7, function(err, value) {
-        if (err) throw err;
-        console.log('The value is ' + value);
-    });
-};
 
-gpio.read(7, function(err, value) {
-    if (err) throw err;
-    console.log('The value is ' + value);
-});
 
 
 app.set('view engine', 'ejs');
